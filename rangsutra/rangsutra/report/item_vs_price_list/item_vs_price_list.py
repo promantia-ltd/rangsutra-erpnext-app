4bd092357facf2b807f55555ba90a308c0b108d3
subramaniac <subramani.ac@promantia.com>
2022-06-13 14:22:03 +0530

    11931/fix in the item vs price list report, sql syntax fix.

--- rangsutra/rangsutra/report/item_vs_price_list/item_vs_price_list.py
+++ rangsutra/rangsutra/report/item_vs_price_list/item_vs_price_list.py
@@ -25,7 +25,6 @@ def get_columns(price_list):
     ]
 
     for k,v in price_list.items():
-        print(k,v)
         columns.append ({
             "label": _(v), "fieldname": k, "width": 140
         })
@@ -40,8 +39,8 @@ def get_data(price_list):
     data = []
 
     for items in item_data:
-        op = f"""select distinct(ip.item_code) as name, ip.price_list_rate as rate, pl.price_list_name as pname, ip.item_name as iname from `tabItem Price` ip 
-                inner join `tabPrice List` as pl on ip.price_list = pl.price_list_name where ip.item_code = "{items.code}" order by ip.item_name desc"""
+        op = """select distinct(ip.item_code) as name, ip.price_list_rate as rate, pl.price_list_name as pname, ip.item_name as iname from `tabItem Price` ip 
+                inner join `tabPrice List` as pl on ip.price_list = pl.price_list_name where ip.item_code = '{items_code}' """.format(items_code=items.code)
         price_data=frappe.db.sql(op, as_dict=True)
 
         dictt = {'item_name': items.name, 'item_code': items.code}
