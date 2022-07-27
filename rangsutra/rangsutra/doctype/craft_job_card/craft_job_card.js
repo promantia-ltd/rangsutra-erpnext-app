abc49da027048f6a1a6973b0f68759d2822788a6
Prasanth <jprasanthkumar28@gmail.com>
2022-07-27 13:27:40 +0530

    12286--> Created the doctypes (dupliacted the Job Order and related child tables)

---
+++ rangsutra/rangsutra/doctype/craft_job_card/craft_job_card.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, rangsutra_app and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Craft Job Card', {
+	// refresh: function(frm) {
+
+	// }
+});
